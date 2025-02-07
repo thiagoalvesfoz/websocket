90d98bd01192fc7474b8cf81cee14977385c0a86
Thiago Alves <thiago.oliveira@swa.com.br>
2025-02-07 17:34:37 -0300

    disable ssl

--- socket-client/src/gateway/socket.ts
+++ socket-client/src/gateway/socket.ts
@@ -4,7 +4,9 @@ import { io } from "socket.io-client";
 const SERVER_URL = 'wss://ec2-54-82-69-77.compute-1.amazonaws.com/chat';
   
 const socket = io(SERVER_URL, {
-  autoConnect: false, // Evita conexão automática
+  transports: ['websocket', 'polling'],
+  rejectUnauthorized: false,
+  autoConnect: false,
 });
 
 export default socket;
