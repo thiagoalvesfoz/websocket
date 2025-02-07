d3df3501eb9ff6f4e77bc1cc92cdee82729fe50c
Thiago Alves <thiago.oliveira@swa.com.br>
2025-02-07 17:40:50 -0300

    ssl

--- socket-client/src/gateway/socket.ts
+++ socket-client/src/gateway/socket.ts
@@ -5,7 +5,6 @@ const SERVER_URL = 'wss://ec2-54-82-69-77.compute-1.amazonaws.com/chat';
   
 const socket = io(SERVER_URL, {
   transports: ['websocket', 'polling'],
-  rejectUnauthorized: false,
   autoConnect: false,
 });
 
