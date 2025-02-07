516a6af18d418862ba3e2d6564d7019832650d99
Thiago Alves <thiago.oliveira@swa.com.br>
2025-02-07 14:01:24 -0300

    feat: env

--- socket-client/src/gateway/socket.ts
+++ socket-client/src/gateway/socket.ts
@@ -1,7 +1,10 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000/chat", {
+const SERVER_URL =
+  process.env.NEX_PUBLIC_SERVER_URL ?? 'ws://localhost:3000/chat';
+
+const socket = io(SERVER_URL, {
   autoConnect: false, // Evita conexão automática
 });
 
