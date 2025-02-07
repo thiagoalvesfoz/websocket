0dc31e1a01d3709fd78996e52a1687331bf9e17c
Thiago Alves <thiago.oliveira@swa.com.br>
2025-02-07 14:22:57 -0300

    add socket url

--- socket-client/src/gateway/socket.ts
+++ socket-client/src/gateway/socket.ts
@@ -1,9 +1,8 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const SERVER_URL =
-  process.env.NEX_PUBLIC_SERVER_URL ?? 'ws://localhost:3000/chat';
-
+const SERVER_URL = 'ws://ec2-54-82-69-77.compute-1.amazonaws.com/chat';
+  
 const socket = io(SERVER_URL, {
   autoConnect: false, // Evita conexão automática
 });
