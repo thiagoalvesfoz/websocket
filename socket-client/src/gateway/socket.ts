ae271667a1292dc71b948d42ca5c78fb4a3b5c4d
Thiago Alves <thiago.oliveira@swa.com.br>
2025-02-07 17:27:32 -0300

    ssl websocket

--- socket-client/src/gateway/socket.ts
+++ socket-client/src/gateway/socket.ts
@@ -1,7 +1,7 @@
 // socket.ts
 import { io } from "socket.io-client";
 
-const SERVER_URL = 'ws://ec2-54-82-69-77.compute-1.amazonaws.com/chat';
+const SERVER_URL = 'wss://ec2-54-82-69-77.compute-1.amazonaws.com/chat';
   
 const socket = io(SERVER_URL, {
   autoConnect: false, // Evita conexão automática
